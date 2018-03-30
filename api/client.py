dc3c4f6867c853042f095a84c515a8f4683da55d
andrearaujo-Daitan <andre.araujo@daitangroup.com>
2018-03-29 23:06:36 -0300

    Adding get all clients and single client, subscribe client, database and my big dick in your fucking butt lols

--- api/client.py
+++ api/client.py
@@ -1,19 +1,46 @@
 import cherrypy
+from sqlalchemy import create_engine
+from flask_jsonpify import jsonify
+import simplejson
+
+db_connect = create_engine('sqlite:///clientbase.db')
 
 @cherrypy.expose
 class Client(object):
     @cherrypy.tools.accept(media='application/json')
 
     @cherrypy.tools.json_out()
-    def GET(self, clients=None):
-        #If clients is null return all, else execute a search query
-        return {"Teste GET":"Success"}
+    def GET(self, clientid=None):
+        if clientid == None:
+            conn = db_connect.connect() # connect to database
+            query = conn.execute("select * from clients") # performs query
+            return {'clients': [i[0] for i in query.cursor.fetchall()]} #fetches the id from all users
+        else:
+            conn = db_connect.connect()
+            query = conn.execute("select * from cients where EmployeeId =%d "  %int(clientid))
+            result = {'data': [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
+            return jsonify(result)
 
     @cherrypy.tools.json_out()
-    def POST(self, clients=None):
-        #Register a client
-        return {"Teste POST":"Success"}
-     
+    @cherrypy.tools.json_in()
+    def POST(self):
+        conn = db_connect.connect()
+        input_json = cherrypy.request.json
+        id = 1                              #como fazer? random?
+        name = input_json["name"]
+        email = input_json["email"]
+        cep = input_json["cep"]
+        phone1 = input_json["phone1"]
+        cpf = input_json["cpf"]
+        sex = input_json["sex"]
+        password = input_json["password"]
+        birthday = input_json["birthday"]
+
+        query = conn.execute("insert into clients (ID, Name, Email, CEP, Phone1, CPF, Password, Birthday)"
+                                             " values ({}, '{}', '{}', '{}', '{}', '{}', '{}' '{}', '{}')"
+                                             .format(id, name, email, cep, phone1, cpf, sex, password, birthday))
+        return jsonify(query.cursor)
+
     @cherrypy.tools.json_out()
     def PUT(self, clients=None):
         #Update clients

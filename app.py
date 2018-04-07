01630ea99813c5e549961e282dd9372e9615c2eb
ivanfilho <ivanfcfilho@gmail.com>
2018-04-06 21:20:03 -0300

    bla

--- app.py
+++ app.py
@@ -16,6 +16,5 @@ if __name__ == '__main__':
 
     cherrypy.tree.mount(Client(),           '/api/client/',     rest_conf)
     cherrypy.tree.mount(UserAccess(),       '/api/useraccess/',  rest_conf)
-    cherrypy.config.update({'server.socket_port': 80})
     cherrypy.engine.start()
     cherrypy.engine.block()

29fb4e92352c56720a326feab2f2803e32b77783
andrearaujo-Daitan <andre.araujo@daitangroup.com>
2018-05-11 21:38:13 -0300

    Retorna ID post useraccess

--- api/user_access.py
+++ api/user_access.py
@@ -48,24 +48,24 @@ class UserAccess(Resource):
         # Get the password in the db
         conn = psycopg2.connect(connect_str)
         cursor = conn.cursor()
-        cursor.execute("select Password from clients where Email='{}'".format(email))
+        cursor.execute("select Password, id from clients where Email='{}'".format(email))
         result = cursor.fetchone()
         if result is None:
             logging.info("Email nao encontrado") # add on log
             return {'Code':1, 'Message':"Password or email do not match"}, 500
         #result = result[0]
         #salt = result[:16]
-        pas_true = str(result)[2:]
+        pas_true = str(result[0])[2:]
         pas_true = pas_true[:-3]
         #pas = argon2.argon2_hash(password, salt)
         
         # Compare the password
-        logging.info("digitado = %s \nverdadeiro = %s", password, pas_true)
         if password == pas_true:
             payload = {'exp':datetime.datetime.utcnow() + datetime.timedelta(hours=1) } #token dura 1h
             token = jwt.encode(payload, secret, algorithm='HS256')
             token = token.decode("utf-8") 
-            json_out = {'token':token}
+            json_out = {'token':token,
+                        'ID': result[1]}
             logging.info("Login com sucesso") # add on log
             return jsonify(json_out)
         logging.info("Senha errada") # add on log
